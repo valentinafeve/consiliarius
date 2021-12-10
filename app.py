2e7486d1367f04cc9264ef3e1667b696063295d2
Valentina <valentina.feruere@mercadolibre.com.co>
2021-12-10 17:16:37 -0500

    feature: Getting pictures from api per seller

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+
+# @app.route('/predict', methods = ['POST'])
+# def update_text():
+#     return
